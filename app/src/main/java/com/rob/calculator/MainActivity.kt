0190029ce3df044777d3b80399fd86f98578cb99
Rob Kokochak <64873559+RobKokochak@users.noreply.github.com>
2023-03-24 18:17:17 -0400

    stable build, still need to implement point and plusMinus buttons

--- app/src/main/java/com/rob/calculator/MainActivity.kt
+++ app/src/main/java/com/rob/calculator/MainActivity.kt
@@ -5,14 +5,17 @@ import android.util.Log
 import android.view.Window
 import androidx.appcompat.app.AppCompatActivity
 import com.rob.calculator.databinding.ActivityMainBinding
+import kotlin.math.sqrt
 
 class MainActivity : AppCompatActivity(), ButtonsFragment.ButtonsListener {
 
     private lateinit var binding: ActivityMainBinding
-    private var operand1 = 0
-    private var operand2 = 0
-    var operation = ""
-    var currentValue = 0
+    private var operand1 = 0.0
+    private var operand2 = 0.0
+    private var operation = ""
+    var result1 = 0.0
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         supportRequestWindowFeature(Window.FEATURE_NO_TITLE)
@@ -25,14 +28,50 @@ class MainActivity : AppCompatActivity(), ButtonsFragment.ButtonsListener {
         Log.i("MainActivity", "in onButtonClick with value " + btnValue)
         val displayFragment = supportFragmentManager.findFragmentById(R.id.displayFragment) as DisplayFragment
 
-        when (btnValue) {
-            "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" -> currentValue = currentValue * 10 + btnValue.toInt()
-            "+", "-", "*", "/", "%", "√" -> operation = btnValue
-
+        fun resetCalc() {
+            operand1 = 0.0
+            operand2 = 0.0
+            operation = ""
         }
-        if (operation == "") operand1 = currentValue
-        else operand2 = currentValue
 
-        displayFragment.changeTextProperties(currentValue.toString())
+        when (btnValue) {
+            "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" -> {
+                if (operation == "") {
+                    operand1 = operand1 * 10 + btnValue.toInt()
+                    displayFragment.changeTextProperties(operand1.toString())
+                }
+                else {
+                    operand2 = operand2 * 10 + btnValue.toInt()
+                    displayFragment.changeTextProperties(operand2.toString())
+                }
+            }
+            // implement plusMinus and point buttons
+
+            "+", "-", "*", "/", "%" -> operation = btnValue
+            "√" -> {
+                result1 = sqrt(operand1)
+                displayFragment.changeTextProperties(result1.toString())
+                resetCalc()
+            }
+            "=" -> {
+                when (operation) {
+                    "+" -> result1 = operand1 + operand2
+                    "-" -> result1 = operand1 - operand2
+                    "*" -> result1 = operand1 * operand2
+                    "/" -> result1 = operand1 / operand2
+                    "%" -> result1 = operand1 % operand2
+                }
+                displayFragment.changeTextProperties(result1.toString())
+                resetCalc()
+                operand1 = result1
+            }
+            "CE" -> {
+                // clears the most recent entry (the last digit you just entered)
+            }
+            "C" -> {
+                resetCalc()
+                displayFragment.changeTextProperties("0")
+            }
+        }
     }
 }
