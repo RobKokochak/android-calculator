581e6249b4aa0e6506984362ea373e085228c02d
Rob Kokochak <64873559+RobKokochak@users.noreply.github.com>
2023-03-25 17:04:27 -0400

    all features implemented, Calculator v1

--- app/src/main/java/com/rob/calculator/DisplayFragment.kt
+++ app/src/main/java/com/rob/calculator/DisplayFragment.kt
@@ -6,18 +6,34 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.rob.calculator.databinding.FragmentDisplayBinding
+import java.math.BigDecimal
+import java.math.RoundingMode
 
 class DisplayFragment : Fragment() {
 
     private lateinit var binding: FragmentDisplayBinding
+    private var displayValue = 0.0
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        super.onCreate(savedInstanceState)
         // Inflate the layout for this fragment
         binding = FragmentDisplayBinding.inflate(inflater, container, false)
+
+        if (savedInstanceState != null) {
+            displayValue = savedInstanceState.getDouble("displayValue")
+            binding.tvDisplay.text = BigDecimal(displayValue).setScale(8, RoundingMode.HALF_EVEN).stripTrailingZeros().toPlainString()
+        }
+
         return binding.root
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        displayValue = binding.tvDisplay.text.toString().toDouble()
+        outState.putDouble("displayValue", displayValue)
     }
 
     fun changeTextProperties(btnVal : String) {
--- app/src/main/java/com/rob/calculator/MainActivity.kt
+++ app/src/main/java/com/rob/calculator/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.rob.calculator
 
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.util.Log
 import android.view.Window
 import androidx.appcompat.app.AppCompatActivity
@@ -14,23 +15,48 @@ import kotlin.math.pow
 class MainActivity : AppCompatActivity(), ButtonsFragment.ButtonsListener {
 
     private lateinit var binding: ActivityMainBinding
+
     private var operand1 = 0.0
     private var operand2 = 0.0
     private var operation = ""
     var result1 = 0.0
     var sign = 1
-    var point = false //false means working on left side of the point
-    var pointPlace = 1
+    private var point = false //false means working on left side of the point
+    private var pointPlace = 1
+    private var displayValue = 0.0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         supportRequestWindowFeature(Window.FEATURE_NO_TITLE)
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        if (savedInstanceState != null) {
+            operand1 = savedInstanceState.getDouble("operand1")
+            operand2 = savedInstanceState.getDouble("operand2")
+            operation = savedInstanceState.getString("operation", "")
+            result1 = savedInstanceState.getDouble("result1")
+            sign = savedInstanceState.getInt("sign")
+            point = savedInstanceState.getBoolean("point")
+            pointPlace = savedInstanceState.getInt("pointPlace")
+            displayValue = savedInstanceState.getDouble("displayValue")
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+        outState.putDouble("operand1", operand1)
+        outState.putDouble("operand2", operand2)
+        outState.putString("operation", operation)
+        outState.putDouble("result1", result1)
+        outState.putInt("sign", sign)
+        outState.putBoolean("point", point)
+        outState.putInt("pointPlace", pointPlace)
+        outState.putDouble("displayValue", displayValue)
     }
 
     override fun onButtonClick(btnValue: String) {
-        Log.i("MainActivity", "in onButtonClick with value " + btnValue)
         val displayFragment = supportFragmentManager.findFragmentById(R.id.displayFragment) as DisplayFragment
 
         fun resetCalc() {
@@ -44,6 +70,10 @@ class MainActivity : AppCompatActivity(), ButtonsFragment.ButtonsListener {
 
         when (btnValue) {
             "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "CE" -> {
+                if (operand1 == result1 && operation == "") { // stops case of editing a previous equation result
+                    resetCalc()
+                    result1 = 0.0
+                }
                 if (operation == "") { //working on 1st operand
                     if (sign == 1) { // positive value
                         if (!point) { // left side of point
@@ -81,7 +111,7 @@ class MainActivity : AppCompatActivity(), ButtonsFragment.ButtonsListener {
                             }
                         }
                     }
-                    displayFragment.changeTextProperties(BigDecimal(operand1).setScale(9, RoundingMode.HALF_EVEN).stripTrailingZeros().toPlainString())
+                    displayValue = operand1
                 }
                 else { // working on 2nd operand
                     if (sign == 1) { // positive value
@@ -120,7 +150,7 @@ class MainActivity : AppCompatActivity(), ButtonsFragment.ButtonsListener {
                             }
                         }
                     }
-                    displayFragment.changeTextProperties(BigDecimal(operand2).setScale(9, RoundingMode.HALF_EVEN).stripTrailingZeros().toPlainString())
+                    displayValue = operand2
                 }
             }
             "." -> {
@@ -130,11 +160,11 @@ class MainActivity : AppCompatActivity(), ButtonsFragment.ButtonsListener {
                 sign *= -1
                 if (operation == "") {
                     operand1 *= -1
-                    displayFragment.changeTextProperties(BigDecimal(operand1).setScale(9, RoundingMode.HALF_EVEN).stripTrailingZeros().toPlainString())
+                    displayValue = operand1
                 }
                 else {
                     operand2 *= -1
-                    displayFragment.changeTextProperties(BigDecimal(operand2).setScale(9, RoundingMode.HALF_EVEN).stripTrailingZeros().toPlainString())
+                    displayValue = operand2
                 }
             }
             "+", "-", "*", "/", "%" -> {
@@ -145,8 +175,9 @@ class MainActivity : AppCompatActivity(), ButtonsFragment.ButtonsListener {
             }
             "√" -> {
                 result1 = sqrt(operand1)
-                displayFragment.changeTextProperties(BigDecimal(result1).setScale(9, RoundingMode.HALF_EVEN).stripTrailingZeros().toPlainString())
+                displayValue = result1
                 resetCalc()
+                operand1 = result1
             }
             "=" -> {
                 when (operation) {
@@ -156,17 +187,17 @@ class MainActivity : AppCompatActivity(), ButtonsFragment.ButtonsListener {
                     "/" -> result1 = operand1 / operand2
                     "%" -> result1 = operand1 % operand2
                 }
-                val formattedResult = BigDecimal(result1).setScale(8, RoundingMode.HALF_EVEN).stripTrailingZeros().toPlainString()
-                displayFragment.changeTextProperties(formattedResult)
+                displayValue = result1
                 resetCalc()
+                operand1 = result1
             }
-            // how to get it so that it saves the answer if I'm immediately operating on it,
-            // but not if I try to enter more numbers on top of it? In that case,
-            // it must reset. like iphone calculator
             "C" -> {
                 resetCalc()
-                displayFragment.changeTextProperties("0")
+                result1 = 0.0
+                displayValue = 0.0
             }
         }
+        if (btnValue == ".") displayFragment.changeTextProperties(BigDecimal(displayValue).setScale(8, RoundingMode.HALF_EVEN).stripTrailingZeros().toPlainString() + ".")
+        else displayFragment.changeTextProperties(BigDecimal(displayValue).setScale(8, RoundingMode.HALF_EVEN).stripTrailingZeros().toPlainString())
     }
 }
